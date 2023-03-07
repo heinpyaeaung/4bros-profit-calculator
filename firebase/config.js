6244b92662aea8e0f1c927ce5180f8673f6e4b48
itsheinpyae <71978117+heinpyaeaung@users.noreply.github.com>
2023-03-07 23:21:55 +0630

    1.2.0

--- firebase/config.js
+++ firebase/config.js
@@ -1,6 +1,7 @@
-const admin = require('firebase-admin')
-admin.initializeApp({
-    credentials: admin.credential.applicationDefault()
-})
+const admin = require('firebase-admin');
+const serviceAccount = require('../key.json')
+admin.initializeApp({    
+    credential: admin.credential.cert(serviceAccount)
+});
 const db = admin.firestore();
 module.exports = db;
